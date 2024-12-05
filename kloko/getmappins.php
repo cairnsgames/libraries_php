1e420b4bf058c9c25702b9ab18e0df6f12e400ad
William Cairns <cairnswm@gmail.com>
2024-12-05 04:47:57 +0200

    Differentiate between keywords and event_type for kloko_events

--- kloko/getmappins.php
+++ kloko/getmappins.php
@@ -19,21 +19,30 @@ if (!isset($lat_nw) || !isset($lng_nw) || !isset($lat_se) || !isset($lng_se)) {
     sendBadRequestResponse("Latitude and longitude parameters are required.");
 }
 
+// Define temporary values for smallest and biggest lat/lng
+$smallestlat = min($lat_nw, $lat_se);
+$biggestlat = max($lat_nw, $lat_se);
+$smallestlng = min($lng_nw, $lng_se);
+$biggestlng = max($lng_nw, $lng_se);
+
 // Fetch data from the database
 $query = "
-SELECT title, '' AS NAME, 'event' AS category, id, image, event_type AS keywords, lat, lng, 'blue' color, '#event' as reference, start_time, end_time
+SELECT title, '' AS NAME, 'event' AS category, id, image, event_type, keywords, lat, lng, 'blue' color, '#event' as reference, start_time, end_time
 FROM kloko_event
-WHERE lat < ? AND lat > ? AND lng > ? AND lng < ?
+WHERE lat < ? AND lat > ? AND lng < ? AND lng > ?
 UNION
-SELECT kl.name, CONCAT(u.firstname, ' ', u.lastname) AS NAME, 'partner' AS pintype, u.id, u.avatar, GROUP_CONCAT(role.name) AS keywords, lat, lng, 'blue' color, '#user' as reference, null, null
+SELECT kl.name, CONCAT(u.firstname, ' ', u.lastname) AS NAME, 'partner' AS pintype, u.id, u.avatar, 'partner', GROUP_CONCAT(role.name) AS keywords, lat, lng, 'blue' color, '#user' as reference, null, null
 FROM kloko_location kl, kloko_user_location ul, user u, user_role, role
-WHERE lat < ? AND lat > ? AND lng > ? AND lng < ? AND ul.user_id = u.id AND kl.id = ul.location_id AND showonmap = 1
+WHERE lat < ? AND lat > ? AND lng < ? AND lng > ? AND ul.user_id = u.id AND kl.id = ul.location_id AND showonmap = 1
 AND user_role.user_id = u.id AND user_role.role_id = role.id
 ";
 
-$result = PrepareExecSQL($query, 'dddddddd', [$lat_nw, $lat_se, $lng_nw, $lng_se, $lat_nw, $lat_se, $lng_nw, $lng_se]);
+$result = PrepareExecSQL($query, 'dddddddd', [$biggestlat, $smallestlat, $biggestlng, $smallestlng, $biggestlat, $smallestlat, $biggestlng, $smallestlng]);
 
-$data = $result;
+// Filter results for valid numerical IDs
+$data = array_filter($result, function($item) {
+    return isset($item['id']) && is_numeric($item['id']);
+});
 
 header('Content-Type: application/json');
 echo json_encode($data);
