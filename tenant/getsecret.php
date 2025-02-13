462449c8471cb66dddc2fc76e4ac6005d62d27f3
William Cairns <cairnswm@gmail.com>
2025-02-13 21:11:42 +0200

    Chnage to use client domain

--- tenant/getsecret.php
+++ tenant/getsecret.php
@@ -1,23 +1,34 @@
 <?php
-include_once dirname(__FILE__)."/../corsheaders.php";
-include_once dirname(__FILE__)."/../dbconfig.php";
-include_once dirname(__FILE__)."/../dbutils.php";
-include_once dirname(__FILE__)."/../apicore/apicore.php";
-include_once dirname(__FILE__)."/../utils.php";
+include_once dirname(__FILE__) . "/../corsheaders.php";
+include_once dirname(__FILE__) . "/../dbconfig.php";
+include_once dirname(__FILE__) . "/../dbutils.php";
+include_once dirname(__FILE__) . "/../apicore/apicore.php";
+include_once dirname(__FILE__) . "/../utils.php";
 
-function getSecret($appid = null, $domain = null) {
+function getSecret($appid = null, $domain = null)
+{
     if ($appid === null) {
         $appid = getAppId();
     }
-    
+
     if ($appid === "NONE") {
         return null;
     }
-    
+
     if ($domain === null) {
-        $domain = explode(':', $_SERVER['HTTP_ORIGIN'])[0];
+        if (!empty($_SERVER['HTTP_ORIGIN'])) {
+            // Get the domain from the Origin header (best option)
+            $clientDomain = parse_url($_SERVER['HTTP_ORIGIN'], PHP_URL_HOST);
+        } elseif (!empty($_SERVER['HTTP_REFERER'])) {
+            // Fallback to the Referer header
+            $clientDomain = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+        } else {
+            // No origin or referer found
+            $clientDomain = 'Unknown';
+        }
+        $domain = $clientDomain;
     }
-    
+
     $sql = "SELECT * FROM application_secret ap
             WHERE app_id = ? 
             AND (domain = ? OR (domain IS NULL AND NOT EXISTS 
@@ -27,6 +38,6 @@ function getSecret($appid = null, $domain = null) {
                 WHERE ap.name = ap2.name AND 
                 app_id = ? AND domain = ?
             )))";
-    
+
     return PrepareExecSQL($sql, "ssss", [$appid, $domain, $appid, $domain]);
 }
