773ff731774cbfecde5d4365f4e3bbff83944a90
William Cairns <cairnswm@gmail.com>
2025-04-04 09:51:54 +0200

    Only return events, not classes for home page

--- kloko/kloko.php
+++ kloko/kloko.php
@@ -149,7 +149,7 @@ function getUpcomingEvents($data)
 {
     global $appId, $EVENT_FIELDS;
     $fields = implode(",", $EVENT_FIELDS);
-    $sql = "select $fields from kloko_event where end_time > ? and app_id = ? order by start_time limit 10";
+    $sql = "select $fields from kloko_event where end_time > ? and app_id = ?  and event_type = 'event' order by start_time limit 10";
     $params = [date("Y-m-d H:i:s"), $appId];
     $sss = "ss";
     $result = PrepareExecSQL($sql, $sss, $params);
