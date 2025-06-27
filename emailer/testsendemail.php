d41b6e97809a16920ef9e0fef9cdbb5f518f726e
William Cairns <cairnswm@gmail.com>
2025-06-27 06:49:28 +0200

    changes to itemseen to allow bulk updates, to reduce application chattiness

--- emailer/testsendemail.php
+++ emailer/testsendemail.php
@@ -4,12 +4,8 @@ include_once dirname(__FILE__) . "/sendemail.php";
 // Example usage:
 $appid = "b0181e17-e5c6-11ee-bb99-1a220d8ac2c9";
 $toEmail = 'cairnswm@gmail.com';
-$subject = 'New Booking: Love Fest';
-$htmlContent = '<div>There is a new booking for the <strong>Love Fest</strong>
-  <div>Name: William Cairns</div>
-  <div>Email: test@te.st</div>
-  <div>Phone: 1234567890</div>
-</div>';
+$subject = 'Test Resend';
+$htmlContent = '<div>This is a test email</div>';
 
 $result = sendEmail($appid, $toEmail, $subject, $htmlContent);
 
--- tracker/itemseen.php
+++ tracker/itemseen.php
@@ -4,21 +4,36 @@ include_once dirname(__FILE__)."/../dbutils.php";
 include_once dirname(__FILE__)."/../utils.php";
 
 $appid = getAppId();
+$items = getParam("items", []);
+$type = getParam("type", "");
 $user_id = getParam("user_id", "");
-$itemid = getParam("id", "");
-$itemtype = getParam("type", "");
-$out = ["itemtype"=>$itemtype, "itemid"=>$itemid, "user_id"=>$user_id];
+$id = getParam("id", "");
+$out = ["processed" => 0];
 
-if ($itemtype != "" && $itemid != "" && $user_id != "") {
-	$sql = "insert into itemseen (app_id, itemtype, user_id, item_id) values (?,?,?,?)
-	  on duplicate key update seennumber = IF(seenlast <= DATE_SUB(NOW(), INTERVAL 15 minute), seennumber + 1, seennumber)";
-	$params = array($appid, $itemtype, $user_id, $itemid);
-	$result = PrepareExecSQL($sql, "ssss", $params);
-	$out["message"] = "itemseen";
-	$out["data"] = $result;
+if (!empty($items)) {
+    foreach ($items as $item) {
+        $itemtype = $item["type"] ?? "";
+        $user_id = $item["user_id"] ?? "";
+        $itemid = $item["id"] ?? "";
+
+        if ($itemtype != "" && $itemid != "" && $user_id != "") {
+            $sql = "insert into itemseen (app_id, itemtype, user_id, item_id) values (?,?,?,?)
+              on duplicate key update seennumber = IF(seenlast <= DATE_SUB(NOW(), INTERVAL 15 minute), seennumber + 1, seennumber)";
+            $params = array($appid, $itemtype, $user_id, $itemid);
+            PrepareExecSQL($sql, "ssss", $params);
+            $out["processed"]++;
+        }
+    }
+} elseif ($type != "" && $id != "" && $user_id != "") {
+    $sql = "insert into itemseen (app_id, itemtype, user_id, item_id) values (?,?,?,?)
+      on duplicate key update seennumber = IF(seenlast <= DATE_SUB(NOW(), INTERVAL 15 minute), seennumber + 1, seennumber)";
+    $params = array($appid, $type, $user_id, $id);
+    PrepareExecSQL($sql, "ssss", $params);
+    $out["processed"]++;
 } else {
-	$out["error"] = "All parameters are required!";
+    $out["error"] = "Invalid input!";
 }
+
 http_response_code(200);
 echo json_encode($out);
 
